6f6ce992c87886f9dc58358db99a13d86e17f7b1
Alex <alex@tuerxun.com>
2025-03-23 16:27:24 -0700

    Add shape drawing functionality with random colors
    Introduced a Turtle-based program to draw shapes with varying sides and random colors based on user input. Removed an unused configuration option from the IDE settings file for cleanup.

---
+++ day_18/main.py
@@ -0,0 +1,34 @@
+import random
+from random import randrange
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+
+def random_color():
+    color = tuple(random.randint(0, 255) for i in range(3))
+    return color
+
+
+def shape_drawer(sides):
+    for i in range(sides):
+        timmy.forward(100)
+        timmy.right(360 / sides)
+    # noinspection PyTypeChecker
+    timmy.pencolor(random_color())
+
+
+def main():
+    timmy.shape("turtle")
+    screen = Screen()
+    screen.colormode(255)
+
+    requested_sides = int(input("How many sides would you like to go up to? "))
+    for shape_sides in range(3, requested_sides + 1):
+        shape_drawer(shape_sides)
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
